b364ff6d961926c25f78de4d9b06f642f5f09054
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-25 16:11:24 +0530

    Add UserHomeForm

---
+++ src/controller/UserHomeFormController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class UserHomeFormController {
+
+    public Button btnRecordAttendance;
+    public Button btnViewReports;
+    public Button btnUserProfile;
+    public Button btnSignOut;
+
+    public void initialize(){
+
+    }
+
+    public void btnRecordAttendance_OnAction(ActionEvent event) {
+
+    }
+
+
+    public void btnSignOut_OnAction(ActionEvent event) {
+
+    }
+
+
+    public void btnUserProfile_OnAction(ActionEvent event) {
+
+    }
+
+
+    public void btnViewReports_OnAction(ActionEvent event) {
+
+    }
+
+}
