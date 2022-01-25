57b646b073049bbe0d93adcc44529b3e1911d3df
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-25 15:35:46 +0530

    Add LoginForm

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    public PasswordField txtPassword;
+    public Button btnSignIn;
+    public TextField txtUserName;
+
+    public void initialize(){
+
+    }
+
+    public void btnSignIn_OnAction(ActionEvent actionEvent) {
+    }
+}
--- src/controller/SplashScreenFormController.java
+++ src/controller/SplashScreenFormController.java
@@ -1,11 +1,41 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SplashScreenFormController {
     public Label lblStatus;
 
     public void initialize(){
+        new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Platform.runLater(()->{
+                try {
+                    Stage stage = new Stage();
+                    AnchorPane root = FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.sizeToScene();
+                    stage.setTitle("Student Attendance: Sign In");
+                    stage.setResizable(false);
+                    stage.show();
+                    stage.centerOnScreen();
+                    ((Stage)lblStatus.getScene().getWindow()).close();
+                } catch ( IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }).start();
 
     }
 }
