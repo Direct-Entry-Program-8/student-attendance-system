a7f01b47dff5d988cc39054fdb4a4de34f735e27
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-25 15:46:24 +0530

    Add CreateAdminForm

---
+++ src/controller/CreateAdminFormController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAdminFormController {
+
+    public TextField txtName;
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public PasswordField txtConfirmPassword;
+    public Button btnCreateAccount;
+
+    public void initialize(){
+
+    }
+
+    public void btnCreateAccount_OnAction(ActionEvent event) {
+
+    }
+
+}
