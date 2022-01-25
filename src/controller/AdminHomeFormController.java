f9f1c46a23e6892841e078569b5a1df1ebed79a3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-25 16:19:53 +0530

    Add AdminHomeForm

---
+++ src/controller/AdminHomeFormController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminHomeFormController {
+
+    
+    public Button btnRecordAttendance;
+
+    
+    public Button btnViewReports;
+
+    
+    public Button btnUserProfile;
+
+    
+    public Button btnManageUsers;
+
+    
+    public Button btnBackupRestore;
+
+    
+    public Button btnSignOut;
+
+    
+    public void btnBackupRestore_OnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnManageUsers_OnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnRecordAttendance_OnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnSignOut_OnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnUserProfile_OnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnViewReports_OnAction(ActionEvent event) {
+
+    }
+
+}
