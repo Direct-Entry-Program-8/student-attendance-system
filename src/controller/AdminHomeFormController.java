8899f8f7f8aa4d45fc57493dcbe0853cdb83af7d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-25 17:43:03 +0530

    Revamp AdminHomeForm

--- src/controller/AdminHomeFormController.java
+++ src/controller/AdminHomeFormController.java
@@ -3,28 +3,39 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class AdminHomeFormController {
 
-    
     public Button btnRecordAttendance;
-
-    
     public Button btnViewReports;
-
-    
     public Button btnUserProfile;
-
-    
     public Button btnManageUsers;
-
-    
     public Button btnBackupRestore;
-
-    
     public Button btnSignOut;
+    public Label lblHover;
 
-    
+    public void initialize(){
+        final String initialText = lblHover.getText();
+        btnRecordAttendance.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnViewReports.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnUserProfile.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnManageUsers.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnBackupRestore.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+        btnSignOut.setOnMouseEntered(event -> displayHoveringText((Button) event.getSource()));
+
+        btnRecordAttendance.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnViewReports.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnUserProfile.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnManageUsers.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnBackupRestore.setOnMouseExited(event -> lblHover.setText(initialText));
+        btnSignOut.setOnMouseExited(event -> lblHover.setText(initialText));
+    }
+
+    private void displayHoveringText(Button button){
+        lblHover.setText(button.getAccessibleText());
+    }
+
     public void btnBackupRestore_OnAction(ActionEvent event) {
 
     }
