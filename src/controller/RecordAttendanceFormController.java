b3a1968b8b8e80df55f2fd98021469d4d7bda6c7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-01-27 17:20:34 +0530

    Update date and time

--- src/controller/RecordAttendanceFormController.java
+++ src/controller/RecordAttendanceFormController.java
@@ -1,11 +1,17 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.util.Date;
 
 public class RecordAttendanceFormController {
 
@@ -18,20 +24,26 @@ public class RecordAttendanceFormController {
     public Label lblName;
     public Label lblStatus;
 
-    public void initialize(){
+    public void initialize() {
+        lblDate.setText(String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %1$Tp", new Date()));
 
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), (event -> {
+            lblDate.setText(String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %1$Tp", new Date()));
+        })));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     public void btnIn_OnAction(ActionEvent event) {
 
     }
 
-    
+
     public void btnOut_OnAction(ActionEvent event) {
 
     }
 
-    
+
     public void txtStudentID_OnAction(ActionEvent event) {
 
     }
