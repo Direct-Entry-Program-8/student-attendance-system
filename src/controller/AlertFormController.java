3e1e886de02bf44933d66c8ee9711e360e22aaae
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-02-22 14:02:17 +0530

    Record Attendance - 5

--- src/controller/AlertFormController.java
+++ src/controller/AlertFormController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javafx.animation.ScaleTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
@@ -17,15 +21,25 @@ public class AlertFormController {
     public Label lblId;
     public Label lblName;
     public Label lblDate;
+    public ImageView imgDanger;
+    public Label lblAlert;
+    public Label lblDescription;
 
     public void initialize() throws URISyntaxException {
+        ScaleTransition st = new ScaleTransition(Duration.millis(400), imgDanger);
+        st.setFromX(0.8);
+        st.setFromY(0.8);
+        st.setToX(1.2);
+        st.setToY(1.2);
+        st.setCycleCount(-1);
+        st.play();
+
         playSiren();
     }
 
     private void playSiren() throws URISyntaxException {
         Media media = new Media(this.getClass().getResource("/assets/siren.mp3").toURI().toString());
         MediaPlayer player = new MediaPlayer(media);
-        player.setCycleCount(2);
         player.play();
     }
 
